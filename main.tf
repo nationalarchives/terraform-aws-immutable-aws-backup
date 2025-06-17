58a3e851653551a286c6817c9948bbf2e82ab111
SaketGhai <saketindago27@gmail.com>
2025-06-17 10:43:46 +0100

    DABACKUPS-66: Added required_provider_version and terraform required_version

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.9.0" 
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 locals {
   # Internal
   account_id                                        = data.aws_caller_identity.current.account_id
