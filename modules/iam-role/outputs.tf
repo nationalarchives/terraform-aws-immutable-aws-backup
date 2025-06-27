4d33f0e207743420236a6eba67aaee3658d50103
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2025-06-27 14:56:12 +0100

    Update outputs.tf

--- modules/iam-role/outputs.tf
+++ modules/iam-role/outputs.tf
@@ -2,7 +2,7 @@ output "role" {
   description = "The IAM role created by this module."
   value       = aws_iam_role.this
 
-  #Important: Ensures that the policies stay on the role until the role can be deleted.
+  # Important: Ensures that the policies stay on the role until the role is no longer needed.
   depends_on = [
     aws_iam_role_policy.this,
     aws_iam_role_policy_attachment.this
