aacee3d084ce642034d52a3874919f6178710be3
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2025-06-27 14:52:09 +0100

    Fix Terraform's dependency graph during a destroy action Ensures that the Deployment Helper Lambda can still be invoked and has the correct permissions until the end.

--- modules/iam-role/outputs.tf
+++ modules/iam-role/outputs.tf
@@ -1,4 +1,10 @@
 output "role" {
   description = "The IAM role created by this module."
   value       = aws_iam_role.this
+
+  #Important: Ensures that the policies stay on the role until the role can be deleted.
+  depends_on = [
+    aws_iam_role_policy.this,
+    aws_iam_role_policy_attachment.this
+  ]
 }
