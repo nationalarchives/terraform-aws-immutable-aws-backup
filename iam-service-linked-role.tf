e1eb5069026befe132df79e504ae8db35afff519
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2025-07-10 00:01:57 +0100

    Fix IAM Service Linked Role always creating (#41)
    The expression inside the try() was always throwing an error as the expected tags didn't exist.

--- iam-service-linked-role.tf
+++ iam-service-linked-role.tf
@@ -12,22 +12,20 @@ locals {
 }
 
 data "aws_iam_role" "potential_backup_service_linked_role" {
-  count = local.potential_backup_service_linked_role_arn == null ? 0 : 1
-  name  = local.potential_backup_service_linked_role_name
+  count = local.potential_backup_service_linked_role_name == null ? 0 : 1
+
+  name = local.potential_backup_service_linked_role_name
 }
 
 resource "aws_iam_service_linked_role" "backup_service_linked_role" {
-  count = try(
-    data.aws_iam_role.potential_backup_service_linked_role[0].tags["terraform"] == "true" &&
-    data.aws_iam_role.potential_backup_service_linked_role[0].tags["deployment_region"] == data.aws_region.current.region ? 1 : 0, 1
-  )
+  count = try(lookup(data.aws_iam_role.potential_backup_service_linked_role[0].tags, "terraform", null) == "true" && lookup(data.aws_iam_role.potential_backup_service_linked_role[0].tags, "deployment_region", null) == local.region ? 1 : 0, 1)
 
   aws_service_name = "backup.amazonaws.com"
   description      = "Service-linked role for AWS Backup"
 
   tags = {
     "terraform"         = "true",
-    "deployment_region" = data.aws_region.current.region,
+    "deployment_region" = local.region
   }
 }
 
