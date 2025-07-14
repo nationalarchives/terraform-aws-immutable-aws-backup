b01aefe9847f2e8d5d5ab32d325bc5ece396a278
Paul Schwarzenberger <paul@celidor.net>
2025-07-14 11:41:30 +0100

    Add deployment identifier - part one

--- iam-service-linked-role.tf
+++ iam-service-linked-role.tf
@@ -7,6 +7,8 @@ data "aws_iam_roles" "backup_service_linked_roles" {
 }
 
 locals {
+  deployment_identifier = trim(var.central_account_resource_name_prefix, "-/")
+
   potential_backup_service_linked_role_arn  = one(data.aws_iam_roles.backup_service_linked_roles.arns)
   potential_backup_service_linked_role_name = one(data.aws_iam_roles.backup_service_linked_roles.names)
 }
@@ -18,14 +20,18 @@ data "aws_iam_role" "potential_backup_service_linked_role" {
 }
 
 resource "aws_iam_service_linked_role" "backup_service_linked_role" {
-  count = try(lookup(data.aws_iam_role.potential_backup_service_linked_role[0].tags, "terraform", null) == "true" && lookup(data.aws_iam_role.potential_backup_service_linked_role[0].tags, "deployment_region", null) == local.region ? 1 : 0, 1)
+  count = try(
+    lookup(data.aws_iam_role.potential_backup_service_linked_role[0].tags, "terraform", null) == "true" &&
+    lookup(data.aws_iam_role.potential_backup_service_linked_role[0].tags, "deployment_region", null) == local.region ? 1 : 0, 1
+  )
 
   aws_service_name = "backup.amazonaws.com"
   description      = "Service-linked role for AWS Backup"
 
   tags = {
-    "terraform"         = "true",
-    "deployment_region" = local.region
+    "terraform"             = "true",
+    "deployment_region"     = local.region,
+    "deployment_identifier" = local.deployment_identifier
   }
 }
 
