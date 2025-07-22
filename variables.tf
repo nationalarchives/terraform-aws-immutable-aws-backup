f5be9ddb7de762150485b7d8e4ee8c34af92a5a0
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2025-07-22 09:46:00 +0100

    Default `allow_backup_targets_to_restore` to `false`

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "central_account_resource_name_prefix" {
 variable "deployments" {
   type = map(object({
     admin_role_names                = optional(list(string), []) # Names of IAM roles that have admin access to the deployment. E.g. can manage the backup vaults in member accounts.
-    allow_backup_targets_to_restore = bool,
+    allow_backup_targets_to_restore = optional(bool, false)
     backup_tag_key                  = optional(string)
     backup_targets                  = list(string)
     max_retention_days              = optional(number)
@@ -15,10 +15,10 @@ variable "deployments" {
       complete_backup_window_minutes        = optional(number)
       continuous_backup_schedule_expression = optional(string, "cron(0 0 ? * * *)") # Schedule for creating continuous backups, if enabled.
       create_continuous_backups             = optional(bool, false)                 # Create continuous backups for resources that support it to enable local PITR, there is no copy action for these backups.
-      intermediate_retention_days           = optional(number),                     # Number of days to retain backups in the intermediate vault.
-      local_retention_days                  = optional(number),                     # Number of days to retain backups in the member account vault. If not specified, defaults to delete_after_days.
+      intermediate_retention_days           = optional(number)                      # Number of days to retain backups in the intermediate vault.
+      local_retention_days                  = optional(number)                      # Number of days to retain backups in the member account vault. If not specified, defaults to delete_after_days.
       require_plan_name_resource_tag        = optional(bool, true)
-      snapshot_from_continuous_backups      = optional(bool, true), # Generate continuous backups for resources that support it and then snapshot from them. These backups do not copy but act as a source for the backup jobs created by the rules. Currently only S3 is supported.
+      snapshot_from_continuous_backups      = optional(bool, true) # Generate continuous backups for resources that support it and then snapshot from them. These backups do not copy but act as a source for the backup jobs created by the rules. Currently only S3 is supported.
       start_backup_window_minutes           = optional(number)
       use_logically_air_gapped_vault        = optional(bool, false)
       rules = list(object({
@@ -27,13 +27,13 @@ variable "deployments" {
         intermediate_retention_days    = optional(number) # Number of days to retain backups in the intermediate vault, overrides the plan's intermediate_retention_days.
         local_retention_days           = optional(number) # Number of days to retain backups in the member account vault. If not specified, defaults to delete_after_days.
         name                           = optional(string)
-        schedule_expression            = string,
+        schedule_expression            = string
         start_backup_window_minutes    = optional(number)
       }))
     }))
     retained_vaults = optional(list(object({
-      min_retention_days             = number,
-      max_retention_days             = number,
+      min_retention_days             = number
+      max_retention_days             = number
       use_logically_air_gapped_vault = optional(bool, false)
     })), [])
   }))
